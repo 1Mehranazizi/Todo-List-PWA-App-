34f4fc44a122e1b703fe73cd0a893522ffa85a21
Mehran <info.mehranazizi@yahoo.com>
2022-06-03 02:52:40 -0700

    Save Todos In LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,18 @@ import { todoModel } from "./model/model";
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<todoModel[]>([]);
+  const [todos, setTodos] = useState<todoModel[]>(() => {
+    const saveItem = localStorage.getItem("todos");
+    if (saveItem) {
+      return JSON.parse(saveItem);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div>
@@ -21,7 +32,12 @@ const App = () => {
       />
       <ul className="todo-container">
         {todos.map((item) => (
-          <TodoItem key={item.id} todo={item} setTodos={setTodos} todos={todos} />
+          <TodoItem
+            key={item.id}
+            todo={item}
+            setTodos={setTodos}
+            todos={todos}
+          />
         ))}
       </ul>
     </div>
