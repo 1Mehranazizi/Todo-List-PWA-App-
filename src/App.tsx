4d402500dc22e3dcbc245d694c4d62555a76eca2
Mehran <info.mehranazizi@yahoo.com>
2022-06-03 02:24:38 -0700

    Set add todos handler

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import { todoModel } from './model/model';
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<todoModel[]>([]);
 
   return (
     <div>
-      <TodoInput todo={todo} setTodo={setTodo} />
+      <TodoInput todo={todo} setTodo={setTodo} setTodos={setTodos} todos={todos} />
     </div>
   );
 };
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React from "react";
+import { todoModel } from "../model/model";
 import styles from "./TodoInput.module.css";
 
 interface Props {
-    todo:string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>
+  todo: string;
+  todos: todoModel[];
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  setTodos: React.Dispatch<React.SetStateAction<todoModel[]>>;
 }
 
-const TodoInput:React.FC<Props> = ({todo,setTodo}) => {
-    return (
-        <form className={styles.todoInput}>
-            <input type="text" placeholder='Your Todo...' value={todo} onChange={(e) => setTodo(e.target.value)} />
-            <button className={styles.btn__todo_input}>Add Todo</button>
-        </form>
-    );
+const TodoInput: React.FC<Props> = ({ todo, setTodo, todos, setTodos }) => {
+  const addTodoHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  return (
+    <form className={styles.todoInput} onSubmit={addTodoHandler}>
+      <input
+        type="text"
+        placeholder="Your Todo..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className={styles.btn__todo_input} type="submit">
+        Add Todo
+      </button>
+    </form>
+  );
 };
 
 export default TodoInput;
